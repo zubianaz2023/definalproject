7ca561e9eee279c057af6850cc828a465123a229
zubianaz2023 <zubia.gist@gmail.com>
2024-06-03 15:03:56 +0900

    some commits

--- api/app.py
+++ api/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/api/')
 def hello_world():
     return "<p>Hello, World!</p>"
-@app.route('api/test')
+@app.route('/api/test')
 def test():
     return "<p>Testing New Route</p>"
