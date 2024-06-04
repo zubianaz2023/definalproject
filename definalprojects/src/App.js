d41647ae1903c53b6579c735e25892df53c97d26
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-05 00:42:29 +0900

    Update App.js

--- definalprojects/src/App.js
+++ definalprojects/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter, Route, Routes, Link } from 'react-router-dom';
 
 import RecommendComponent from './RecommendComponent';
 import Restaurant from './Restaurant';
