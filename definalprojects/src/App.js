8e1b96fcdb4037bcbd56acb2b0ec349fea0f6dee
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-05 00:22:29 +0900

    Update App.js

--- definalprojects/src/App.js
+++ definalprojects/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes,Link } from 'react-router-dom';
+import { HashRouter , Route, Routes,Link } from 'react-router-dom';
 import RecommendComponent from './RecommendComponent';
 import Restaurant from './Restaurant';
 import config from './config';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div>
         <h1>Best Restaurants in the Gwangju</h1>
         <Routes>
           <Route path="/" element={<Restaurant />} />
-          <Route path="/recommends" element={<RecommendComponent />} />
+          <Route path="/recommend" element={<RecommendComponent />} />
           
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
