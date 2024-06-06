b8cf93689dac281711a4732a207f0fd6b5f14791
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-06 14:53:45 +0900

    Update RecommendComponent.js

--- definalprojects/src/RecommendComponent.js
+++ definalprojects/src/RecommendComponent.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import config from './config'; // Ensure this imports the correct config file
 
 function RecommendComponent() {
   const query = new URLSearchParams(useLocation().search);
   const longitude = query.get('longitude');
   const latitude = query.get('latitude');
   const [recommendedRestaurants, setRecommendedRestaurants] = useState([]);
+  const [topResData, setTopResData] = useState([]);
+
+  useEffect(() => {
+    // Fetch top_res data
+    fetch(`${config.backendUrl}/get_top_res`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.top_res) {
+          setTopResData(data.top_res);
+        } else {
+          console.error('Error fetching top_res data:', data.error);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   useEffect(() => {
     if (longitude && latitude) {
       fetch(`${config.backendUrl}/recommend?longitude=${longitude}&latitude=${latitude}`)
-
         .then(response => response.json())
         .then(data => {
           if (data.recommended_restaurants) {
@@ -23,7 +40,7 @@ function RecommendComponent() {
           console.error('Error fetching data:', error);
         });
     }
-  }, [longitude, latitude]);
+  }, [longitude, latitude, topResData]);
 
   return (
     <div>
