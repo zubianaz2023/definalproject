9455e6a39945d01d27e1692b8a27f0ac9079bb57
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-06 14:53:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,81 +4,57 @@ from sklearn.cluster import KMeans
 import numpy as np
 from flask_cors import CORS
 
-
-# Initialize W&B with your API key
-
-
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://triprecommender.onrender.com"}})
+
 def convert_to_float(value):
     try:
         return float(value)
     except ValueError:
         return None
 
-
 # Load data from CSV
-#print("Loading data from CSV...")
-df = pd.read_csv("Places.csv")
-#print("Sort by ranking position...")
-top = df.sort_values(by=['Rating'], ascending=True)
-
-
-# Load data from CSV
-#print("Loading data from CSV...")
+print("Loading data from CSV...")
 df_res = pd.read_csv("dataset.csv")
-filtered_df = df_res.dropna(subset=['longitude', 'latitude','rankingPosition','image'])
-#print("Sort by ranking position...")
+filtered_df = df_res.dropna(subset=['longitude', 'latitude', 'rankingPosition', 'image'])
 top_res = filtered_df.sort_values(by=['rankingPosition'], ascending=True)
-#Extract coordinates
-#print("Extracting coordinates...")
+
+# Extract coordinates
 coords = top_res[['longitude', 'latitude']]
-# Fit KMeans with k=3
-#print("Fitting KMeans model...")
 kmeans = KMeans(n_clusters=3, init='k-means++')
 kmeans.fit(coords)
 
 top_res['cluster'] = kmeans.labels_
-#print(top_res)
-
 
 def recommend_restaurants(top_res, longitude, latitude):
     cluster = kmeans.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
-   #print("Cluster:", cluster)
-    cluster_df = top_res[top_res['cluster'] == cluster].iloc[:5, [0,3,8,14]]
+    cluster_df = top_res[top_res['cluster'] == cluster].iloc[:5, ['name', 'address', 'image', 'rankingPosition']]
     return cluster_df
-
 
 @app.route('/')
 def get_clusters():
-    # Add other information to the clusters data
-    #print("Preparing clusters data...")
-    clusters_data = top[['Name', 'Rating', 'address', 'image','longitude','latitude']]
-
-    # Convert clusters data to list of dictionaries
-    #print("Converting clusters data to list of dictionaries...")
+    clusters_data = top_res[['name', 'rankingPosition', 'address', 'image', 'longitude', 'latitude']]
     clusters_list = clusters_data.to_dict(orient='records')
-
-    #print("Data processing completed.")
     return jsonify({'clusters': clusters_list})
 
 @app.route('/recommend')
 def recommend():
-    
-    
-    
+    longitude_str = request.args.get('longitude')
+    latitude_str = request.args.get('latitude')
 
-    # Convert longitude and latitude strings to floats
-    #longitude = convert_to_float(longitude_str)
-    #latitude = convert_to_float(latitude_str)
-    print("i think this is not working")
-    data = top_res[['name', 'address','image','phone']]
-    print(data)
+    longitude = convert_to_float(longitude_str)
+    latitude = convert_to_float(latitude_str)
+    
     if longitude is not None and latitude is not None:
         recommended_restaurants = recommend_restaurants(top_res, longitude, latitude)
-        return jsonify({'recommended_restaurants': recommended_restaurants.to_dict(orient='records')
-})
+        return jsonify({'recommended_restaurants': recommended_restaurants.to_dict(orient='records')})
     else:
         return jsonify({'error': 'Invalid longitude or latitude value.'})
+
+@app.route('/get_top_res')
+def get_top_res():
+    top_res_data = top_res.to_dict(orient='records')
+    return jsonify({'top_res': top_res_data})
+
 if __name__ == '__main__':
     app.run(debug=True)
