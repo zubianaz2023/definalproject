5470656fcd03bca530fe77d8bb00acd75bae4257
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-08 00:24:48 +0900

    Update RecommendComponent.js

--- definalprojects/src/RecommendComponent.js
+++ definalprojects/src/RecommendComponent.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import config from './config'; // Ensure this imports the correct config file
-import './RecommendComponent.css'; // Import the CSS file for styling
+
 
 function RecommendComponent() {
   const query = new URLSearchParams(useLocation().search);
