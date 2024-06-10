2232f6d9cec0f055f9d38245ebd50330665db9e5
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-10 11:35:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,16 +32,11 @@ df_hotels = pd.read_csv("Hotels.csv")
 top_hotels = df_hotels.dropna(subset=['image', 'longitude', 'latitude'])
 
 # Extract coordinates for KMeans
-coords_places = top[['longitude', 'latitude']]
 coords_res = top_res[['longitude', 'latitude']]
 coords_malls = top_malls[['longitude', 'latitude']]
 coords_hotels = top_hotels[['longitude', 'latitude']]
 
 # Fit KMeans with k=3 for all datasets
-kmeans_places = KMeans(n_clusters=3, init='k-means++')
-kmeans_places.fit(coords_places)
-top['cluster'] = kmeans_places.labels_
-
 kmeans_res = KMeans(n_clusters=8, init='k-means++')
 kmeans_res.fit(coords_res)
 top_res['cluster'] = kmeans_res.labels_
@@ -53,11 +48,6 @@ top_malls['cluster'] = kmeans_malls.labels_
 kmeans_hotels = KMeans(n_clusters=3, init='k-means++')
 kmeans_hotels.fit(coords_hotels)
 top_hotels['cluster'] = kmeans_hotels.labels_
-
-def recommend_places(top, longitude, latitude):
-    cluster = kmeans_places.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
-    cluster_df = top[top['cluster'] == cluster].iloc[:5, [0, 1, 2, 3, 6, 7]]
-    return cluster_df
 
 def recommend_restaurants(top_res, longitude, latitude):
     cluster = kmeans_res.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
@@ -86,8 +76,23 @@ def get_restaurant_clusters():
     clusters_list = clusters_data.to_dict(orient='records')
     return jsonify({'clusters': clusters_list})
 
+@app.route('/places/get_top_res')
+def get_top_res():
+    top_res_data = top_res[['name', 'address', 'image', 'phone']].to_dict(orient='records')
+    return jsonify({'top_res': top_res_data})
+
+@app.route('/places/get_top_malls')
+def get_top_malls():
+    top_malls_data = top_malls[['name', 'address', 'image', 'phone']].to_dict(orient='records')
+    return jsonify({'top_malls': top_malls_data})
+
+@app.route('/places/get_top_hotels')
+def get_top_hotels():
+    top_hotels_data = top_hotels[['name', 'image', 'phone']].to_dict(orient='records')
+    return jsonify({'top_hotels': top_hotels_data})
+
 @app.route('/places/recommend')
-def recommend_places_endpoint():
+def recommend_places():
     longitude_str = request.args.get('longitude')
     latitude_str = request.args.get('latitude')
 
@@ -98,6 +103,7 @@ def recommend_places_endpoint():
         recommended_restaurants = recommend_restaurants(top_res, longitude, latitude)
         recommended_malls = recommend_malls(top_malls, longitude, latitude)
         recommended_hotels = recommend_hotels(top_hotels, longitude, latitude)
+        
         return jsonify({
             'recommended_restaurants': recommended_restaurants.to_dict(orient='records'),
             'recommended_malls': recommended_malls.to_dict(orient='records'),
@@ -107,7 +113,7 @@ def recommend_places_endpoint():
         return jsonify({'error': 'Invalid longitude or latitude value.'}), 400
 
 @app.route('/restaurants/recommend')
-def recommend_restaurants_endpoint():
+def recommend_restaurants():
     longitude_str = request.args.get('longitude')
     latitude_str = request.args.get('latitude')
 
@@ -115,13 +121,14 @@ def recommend_restaurants_endpoint():
     latitude = convert_to_float(latitude_str)
 
     if longitude is not None and latitude is not None:
-        recommended_places = recommend_places(top, longitude, latitude)
-        recommended_malls = recommend_malls(top_malls, longitude, latitude)
+        recommended_places = recommend_malls(top_malls, longitude, latitude)
         recommended_hotels = recommend_hotels(top_hotels, longitude, latitude)
+        recommended_malls = recommend_malls(top_malls, longitude, latitude)
+
         return jsonify({
             'recommended_places': recommended_places.to_dict(orient='records'),
-            'recommended_malls': recommended_malls.to_dict(orient='records'),
-            'recommended_hotels': recommended_hotels.to_dict(orient='records')
+            'recommended_hotels': recommended_hotels.to_dict(orient='records'),
+            'recommended_malls': recommended_malls.to_dict(orient='records')
         })
     else:
         return jsonify({'error': 'Invalid longitude or latitude value.'}), 400
