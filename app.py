267043d08cb51fe50c59ec2c7897ca3398615948
zubianaz2023 <143669035+zubianaz2023@users.noreply.github.com>
2024-06-10 11:16:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,92 @@ kmeans_res.fit(coords_res)
 top_res['cluster'] = kmeans_res.labels_
 
 kmeans_malls = KMeans(n_clusters=3, init='k-means++')
-kmeans_malls.fit(coords_ma
+kmeans_malls.fit(coords_malls)
+top_malls['cluster'] = kmeans_malls.labels_
+
+kmeans_hotels = KMeans(n_clusters=3, init='k-means++')
+kmeans_hotels.fit(coords_hotels)
+top_hotels['cluster'] = kmeans_hotels.labels_
+
+def recommend_places(top, longitude, latitude):
+    cluster = kmeans_places.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
+    cluster_df = top[top['cluster'] == cluster].iloc[:5, [0, 1, 2, 3, 6, 7]]
+    return cluster_df
+
+def recommend_restaurants(top_res, longitude, latitude):
+    cluster = kmeans_res.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
+    cluster_df = top_res[top_res['cluster'] == cluster].iloc[:5, [0, 2, 8, 9]]
+    return cluster_df
+
+def recommend_malls(top_malls, longitude, latitude):
+    cluster = kmeans_malls.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
+    cluster_df = top_malls[top_malls['cluster'] == cluster].iloc[:5, [0, 1, 2, 3, 4]]
+    return cluster_df
+
+def recommend_hotels(top_hotels, longitude, latitude):
+    cluster = kmeans_hotels.predict(np.array([longitude, latitude]).reshape(1, -1))[0]
+    cluster_df = top_hotels[top_hotels['cluster'] == cluster].iloc[:5, [0, 1, 2, 6, 7]]
+    return cluster_df
+
+@app.route('/places')
+def get_clusters():
+    clusters_data = top[['Name', 'Rating', 'address', 'image', 'longitude', 'latitude']]
+    clusters_list = clusters_data.to_dict(orient='records')
+    return jsonify({'clusters': clusters_list})
+
+@app.route('/restaurants')
+def get_restaurant_clusters():
+    clusters_data = top_res[['name', 'rankingPosition', 'address', 'image', 'longitude', 'latitude']]
+    clusters_list = clusters_data.to_dict(orient='records')
+    return jsonify({'clusters': clusters_list})
+
+@app.route('/places/recommend')
+def recommend_places_endpoint():
+    longitude_str = request.args.get('longitude')
+    latitude_str = request.args.get('latitude')
+
+    longitude = convert_to_float(longitude_str)
+    latitude = convert_to_float(latitude_str)
+
+    if longitude is not None and latitude is not None:
+        recommended_restaurants = recommend_restaurants(top_res, longitude, latitude)
+        recommended_malls = recommend_malls(top_malls, longitude, latitude)
+        recommended_hotels = recommend_hotels(top_hotels, longitude, latitude)
+        return jsonify({
+            'recommended_restaurants': recommended_restaurants.to_dict(orient='records'),
+            'recommended_malls': recommended_malls.to_dict(orient='records'),
+            'recommended_hotels': recommended_hotels.to_dict(orient='records')
+        })
+    else:
+        return jsonify({'error': 'Invalid longitude or latitude value.'}), 400
+
+@app.route('/restaurants/recommend')
+def recommend_restaurants_endpoint():
+    longitude_str = request.args.get('longitude')
+    latitude_str = request.args.get('latitude')
+
+    longitude = convert_to_float(longitude_str)
+    latitude = convert_to_float(latitude_str)
+
+    if longitude is not None and latitude is not None:
+        recommended_places = recommend_places(top, longitude, latitude)
+        recommended_malls = recommend_malls(top_malls, longitude, latitude)
+        recommended_hotels = recommend_hotels(top_hotels, longitude, latitude)
+        return jsonify({
+            'recommended_places': recommended_places.to_dict(orient='records'),
+            'recommended_malls': recommended_malls.to_dict(orient='records'),
+            'recommended_hotels': recommended_hotels.to_dict(orient='records')
+        })
+    else:
+        return jsonify({'error': 'Invalid longitude or latitude value.'}), 400
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'error': 'Not Found'}), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'error': 'Internal Server Error'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
